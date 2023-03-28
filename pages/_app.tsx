9279972501a378a8f555c06a287134b0d6a66979
Thomas Kilgour <thomas@kilgour.co>
2023-03-28 16:57:25 -0400

    reconfigure onesignal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,17 @@
 import "@/styles/globals.css";
-import { useEffect } from "react";
 import type { AppProps } from "next/app";
-import Script from "next/script";
+import { useEffect } from "react";
+import OneSignal from "react-onesignal";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-      OneSignal.init({
-        appId: "e8472fa1-f22c-4943-8147-ce9ba1407c2c",
-        notifyButton: {
-          enable: true,
-        },
-        allowLocalhostAsSecureOrigin: true,
-      });
+    OneSignal.init({
+      appId: "e8472fa1-f22c-4943-8147-ce9ba1407c2c",
+      notifyButton: {
+        enable: true,
+      },
+      allowLocalhostAsSecureOrigin: true,
     });
-
-    return () => {
-      window.OneSignal = undefined;
-    };
   }, []);
-
-  return (
-    <>
-      <Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async />
-      <Component {...pageProps} />;
-    </>
-  );
+  return <Component {...pageProps} />;
 }
