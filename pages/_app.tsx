fa9a3f15f5ace3a66b180352b0417088e2386b71
Thomas Kilgour <thomas@kilgour.co>
2023-03-28 16:40:30 -0400

    add OneSignal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "e8472fa1-f22c-4943-8147-ce9ba1407c2c",
+        notifyButton: {
+          enable: true,
+        },
+        allowLocalhostAsSecureOrigin: true,
+      });
+    });
+
+    return () => {
+      window.OneSignal = undefined;
+    };
+  }, []);
+
+  return (
+    <>
+      <Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async />
+      <Component {...pageProps} />;
+    </>
+  );
 }
